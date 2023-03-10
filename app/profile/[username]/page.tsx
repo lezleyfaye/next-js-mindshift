1b41a73566827e20712b34e8c84d234ccc256194
Lezley Faye <lezleydavidson@gmail.com>
2023-03-10 09:47:34 +0100

    Added html for likert scale

--- app/profile/[username]/page.tsx
+++ app/profile/[username]/page.tsx
@@ -1,5 +1,7 @@
 import { notFound } from 'next/navigation';
 import { getUserByUsername } from '../../../database/users';
+// fix this import path
+import styles from './profile/[username]/page.module.scss';
 
 type Props = { params: { username: string } };
 
@@ -13,6 +15,28 @@ export default async function UserProfile({ params }: Props) {
   return (
     <>
       <h1>Hi {user.username}! How are you today?</h1>
+      <section>
+        <div>
+          <h2>Overall Feeling Today</h2>
+          <table>
+            <tbody>
+              <tr>
+                <td>😔</td>
+                <input type="radio" value="1" name="not good" />
+                <td />
+                <input type="radio" value="2" name="not the worst" />
+                <td />
+                <input type="radio" value="3" name="ok" />
+                <td />
+                <input type="radio" value="4" name="pretty great" />
+
+                <input type="radio" value="5" name="fantastic" />
+                <td>😁</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </section>
     </>
   );
 }
