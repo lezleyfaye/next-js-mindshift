bc485e19a406e91d0c2ea5e994ecb808e2e00ec9
Lezley Faye <lezleydavidson@gmail.com>
2023-03-15 14:11:35 +0100

    Added notes

--- app/profile/[username]/page.tsx
+++ app/profile/[username]/page.tsx
@@ -1,7 +1,7 @@
 import { notFound } from 'next/navigation';
 import { getUserByUsername } from '../../../database/users';
 
-// fix this import path
+// fix this import path, can scss be used on server component pages?
 // import styles from './profile/[username]/page.module.scss';
 
 type Props = { params: { username: string } };
