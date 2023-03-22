3091c662072920de65a2bae0d256e2609e9fb089
Lezley Faye <lezleydavidson@gmail.com>
2023-03-22 16:31:43 +0100

    Added session token to register page

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,7 +1,23 @@
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+import { getValidSessionByToken } from '../../../database/sessions';
 import RegisterForm from './RegisterForm';
 
 type Props = { searchParams: { returnTo?: string | string[] } };
 
-export default function RegisterPage(props: Props) {
+export default async function RegisterPage(props: Props) {
+  // checking valid session
+  const sessionTokenCookie = cookies().get('sessionToken');
+  console.log(sessionTokenCookie);
+
+  const session =
+    sessionTokenCookie &&
+    (await getValidSessionByToken(sessionTokenCookie.value));
+
+  // yes = redirect home
+  if (session) {
+    redirect('/');
+  }
+  // no = return
   return <RegisterForm returnTo={props.searchParams.returnTo} />;
 }
