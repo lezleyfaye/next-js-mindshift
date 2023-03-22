337ceba899e498a25ca7957a3fe370f001977a2d
Lezley Faye <lezleydavidson@gmail.com>
2023-03-22 15:45:30 +0100

    Check if valid session

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,22 @@
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+import { getValidSessionByToken } from '../../../database/sessions';
 import LoginForm from './LoginForm';
 
 type Props = { searchParams: { returnTo?: string | string[] } };
 
-export default function LoginPage(props: Props) {
+export default async function LoginPage(props: Props) {
+  // checking valid session
+  const sessionTokenCookie = cookies().get('sessionToken');
+  // console.log(sessionTokenCookie);
+
+  const session =
+    sessionTokenCookie &&
+    (await getValidSessionByToken(sessionTokenCookie.value));
+  // yes = redirect home
+  if (session) {
+    redirect('/');
+  }
+  // no = return
   return <LoginForm returnTo={props.searchParams.returnTo} />;
 }
--- database/sessions.ts
+++ database/sessions.ts
@@ -1,10 +1,10 @@
 import { cache } from 'react';
 import { sql } from './connect';
 
-// type Session = {
-//   id: number;
-//   token: string;
-// };
+type Session = {
+  id: number;
+  token: string;
+};
 
 export const createSession = cache(async (token: string, userId: number) => {
   const [session] = await sql<{ id: number; token: string }[]>`
@@ -29,4 +29,35 @@ export const deletedExpiredSessions = cache(async () => {
   WHERE
   expiry_timestamp < now()
   `;
+});
+
+export const deleteSessionByToken = cache(async (token: string) => {
+  const [session] = await sql<{ id: number; token: string }[]>`
+    DELETE FROM
+      sessions
+    WHERE
+      sessions.token = ${token}
+    RETURNING
+      id,
+      token
+  `;
+
+  return session;
+});
+
+export const getValidSessionByToken = cache(async (token: string) => {
+  const [session] = await sql<Session[]>`
+    SELECT
+      sessions.id,
+      sessions.token,
+      sessions.csrf_secret
+     FROM
+      sessions
+    WHERE
+      sessions.token = ${token}
+    AND
+      sessions.expiry_timestamp > now()
+  `;
+
+  return session;
 });
