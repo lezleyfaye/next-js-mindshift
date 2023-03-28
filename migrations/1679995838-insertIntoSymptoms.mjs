437e2bede62c765df9ccafe6a026091b6c90bcce
Lezley Faye <lezleydavidson@gmail.com>
2023-03-28 11:58:33 +0200

    Created insertIntoSymptoms

---
+++ migrations/1679995838-insertIntoSymptoms.mjs
@@ -0,0 +1,25 @@
+const symptoms = [
+  { id: 1, name: 'overall' },
+  { id: 2, name: 'sadness' },
+  { id: 3, name: 'anger' },
+  { id: 4, name: 'focus' },
+  { id: 5, name: 'appetite' },
+  { id: 6, name: 'somatic' },
+  { id: 7, name: 'fatigue' },
+  { id: 8, name: 'sleep' },
+];
+
+export async function up(sql) {
+  await sql`
+    INSERT INTO symptoms ${sql(symptoms, 'name')}
+    `;
+}
+
+export async function down(sql) {
+  await sql`
+    DELETE FROM
+    symptoms
+    WHERE
+    id = ${symptoms.id}
+  `;
+}
