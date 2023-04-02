260a15a1963bcea3e24f2301b1ef7be3adb6a503
Lezley Faye <lezleydavidson@gmail.com>
2023-04-02 13:27:48 +0200

    Added filler for tracking page for now

--- app/track/Track.tsx
+++ app/track/Track.tsx
@@ -1,8 +1,25 @@
 'use client';
-import { useState } from 'react';
 
-// import styles from './track.module.scss';
+import styles from './track.module.scss';
 
 export default function TrackPage() {
-  return <h1>track</h1>;
+  return (
+    <div className={styles.trackInfo}>
+      <h1>
+        Do some of your symptoms seem to get worse or improve sporadically? Are
+        any symptoms not getting better? Did you change your medication or
+        dosage?
+        <br />
+        These graphs can be helpful for you to see any trends in your symptoms.
+        Tracking symptoms is best done with the help of a mental health
+        professional.
+      </h1>
+      <img
+        src="/images/dataPage.png"
+        alt="mindshift data page prototype with line graphs"
+        width="356"
+        height="137"
+      />
+    </div>
+  );
 }
